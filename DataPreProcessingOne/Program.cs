2a63fac4c511348c487c3f321309caa478e3bf5b
shravanthi-mikky <shravanthi27041996@gmail.com>
2023-01-03 00:07:53 +0530

    [shravanthi] - Welcome msg

--- DataPreProcessingOne/Program.cs
+++ DataPreProcessingOne/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ML;
+using System;
 
 namespace DataPreProcessingOne
 {
@@ -7,6 +8,16 @@ namespace DataPreProcessingOne
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<PurchaseList>("./data_preprocessing.csv",hasHeader:true,separatorChar:',');
+
+            var split = context.Data.TrainTestSplit(data,testFraction:0.2);
+
+            var dataProcessPipeline = context.Transforms.Concatenate("Features", new[] { "Country", "Age", "Salary", "Purchased" });
+
+            var trainer = context.BinaryClassification.Trainers.
         }
     }
 }
---
+++ DataPreProcessingOne/PurchaseList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ML.Data;
+
+namespace DataPreProcessingOne
+{
+    public class PurchaseList
+    {
+        [LoadColumn(0)]
+        public string Country { get; set; }
+
+        [LoadColumn(1)]
+        public int Age { get; set; }
+
+        [LoadColumn(2)]
+        public int Salary { get; set; }
+
+        [LoadColumn(3)]
+        public string Purchased { get; set; }
+
+    }
+}
